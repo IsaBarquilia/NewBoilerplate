41c83c0f33cceed2eeae86d9c372809c371a9aec
Isabelle Barquilia <isabelle-moraes@aluno.senai>
2024-08-28 13:23:41 -0300

    create styles

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,12 +1,17 @@
 import { useNavigation } from '@react-navigation/native'
 import { View, Text, TouchableOpacity } from 'react-native'
 
+import style from './style'
+
+
 export default function Home() {
   const navigation = useNavigation()
   return (
-    <View>
-      <Text>Home page</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Totoro')}>
+    <View style={style.container}>
+      <Text style={style.title}>Home page</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Totoro')
+      } style={style.button}
+      >
         <Text>Go to Totoro</Text>
       </TouchableOpacity>
     </View>
--- src/screens/Totoro/index.jsx
+++ src/screens/Totoro/index.jsx
@@ -1,12 +1,14 @@
 import { useNavigation } from '@react-navigation/native'
 import { View, Text, TouchableOpacity } from 'react-native'
+import style from './style'
 
 export default function Totoro() {
   const navigation = useNavigation()
   return (
-    <View>
-      <Text>Totoro page</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+    <View style={style.container}>
+      <Text style={style.title}>Totoro page</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Home')}
+        style={style.button}>
         <Text>Back to Home</Text>
         </TouchableOpacity>
     </View>
