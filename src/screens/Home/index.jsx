bdf17d1ebcf9e188540bd9257551801f40bac6c0
Isabelle Barquilia <isabelle-moraes@aluno.senai>
2024-08-28 12:56:30 -0300

    create totoro page 2

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,10 +1,14 @@
-import { View, Text } from 'react-native'
-import { styles } from './styles'
+import { useNavigation } from '@react-navigation/native'
+import { View, Text, TouchableOpacity } from 'react-native'
 
 export default function Home() {
+  const navigation = useNavigation()
   return (
     <View>
       <Text>Home page</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Totoro')}>
+        <Text>Go to Totoro</Text>
+      </TouchableOpacity>
     </View>
   )
 }
--- src/screens/Totoro/index.jsx
+++ src/screens/Totoro/index.jsx
@@ -1,10 +1,14 @@
-import { View, Text } from 'react-native'
-import { styles } from './styles'
+import { useNavigation } from '@react-navigation/native'
+import { View, Text, TouchableOpacity } from 'react-native'
 
 export default function Totoro() {
+  const navigation = useNavigation()
   return (
     <View>
       <Text>Totoro page</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+        <Text>Back to Home</Text>
+        </TouchableOpacity>
     </View>
   )
 }
