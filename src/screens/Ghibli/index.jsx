d6e7fb0efe52b97f03538e3e43ae32135175fc2b
Isabelle Barquilia <isabelle-moraes@aluno.senai>
2024-08-28 15:27:12 -0300

    create page ghibli

--- src/screens/Ghibli/index.jsx
+++ src/screens/Ghibli/index.jsx
@@ -1,16 +1,17 @@
 import { useNavigation } from '@react-navigation/native'
-import { View } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import Button from '../../components/Button'
 import style from './style'
 import Title from '../../components/Title'
 
-export default function Home() {
 
+export default function Ghibli() {
+  const navigation = useNavigation()
   return (
     <View style={style.container}>
-      <Title title={"Home page"} />
+      <Title title={"Ghibli page"} />
+      <Button screen={"Home"} name={"Go to Home"}/>
       <Button screen={"Totoro"} name={"Go to Totoro"}/>
-      <Button screen={"Ghibli"} name={"Go to Ghibli"}/>
     </View>
   )
 }
